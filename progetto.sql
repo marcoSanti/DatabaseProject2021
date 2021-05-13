63dc33456a1acc90a176a6b5c1460491d86b3a8b
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2021-05-13 17:03:19 +0200

    operazioni sql inserite

--- progetto.sql
+++ progetto.sql
@@ -183,6 +183,10 @@ insert into Cittadino values ('LSSGGI81H03F205A', 'Alessandro', 'Gigio', 40, 'Mi
 insert into Cittadino values ('LCUPSL76E52D969X', 'Lucia', 'Pensaldo', 45, 'Genova', 'Via del vento 100', 'Fragili', false, '');
 insert into Cittadino values ('MSSBLD45L23L483O', 'Massimo', 'Boldi', 76, 'Udine', 'Via cipollina 16', 'Altro', true, '');
 
+insert into Cittadino values ('PLLFRC80A41L219R', 'Federica', 'Pellegrini', 41, 'Torino', 'Via nuoto 1', 'Sanitario', false, '');
+insert into Cittadino values ('GNTRMS00A41D969W', 'Artemisia', 'Gentileschi', 60, 'Genova', 'Via degli artisti poveri 69', 'Sanitario', false, '');
+insert into Cittadino values ('CRSSNT95A41L483B', 'Samantha', 'Cristoforetti', 26, 'Udine', 'Via dei falchi 600', 'Sanitario' ,false, '');
+
 
 insert into Vaccino values ('COVIDIN', 75, 99, 16, 20 ,2);
 insert into Vaccino values ('FLUSTOP', 90, 99, 18, NULL, 1);
@@ -192,6 +196,7 @@ insert into Vaccino values ('CORONAX', 60, 99, 60, 20, 2);
 insert into Lotto values ('COV_1234', TO_DATE('18/05/2021', 'DD/MM/YYYY'), TO_DATE('01/08/2021', 'DD/MM/YYYY'));
 insert into Lotto values ('FLU_5678', TO_DATE('12/02/2021', 'DD/MM/YYYY'), TO_DATE('01/10/2021', 'DD/MM/YYYY'));
 insert into Lotto values ('COR_0122', TO_DATE('14/03/2021', 'DD/MM/YYYY'), TO_DATE('31/08/2021', 'DD/MM/YYYY'));
+insert into Lotto values ('COR_0123', TO_DATE('14/03/2021', 'DD/MM/YYYY'), TO_DATE('31/08/2021', 'DD/MM/YYYY'));
 
 
 insert into CentroVaccinale values ('Torino', 'Corso palestro 12');
@@ -201,22 +206,30 @@ insert into CentroVaccinale values ('Udine', 'Via Garibaldi 1');
 
 
 insert into Medico values ('NCLTMB83H03L219S', 'Milano', 'Corso Svizzera 92', 'Altro');
+insert into Medico values ('PLLFRC80A41L219R', 'Torino', 'Corso palestro 12', 'Altro');
+insert into Medico values ('GNTRMS00A41D969W', 'Genova', 'Via Roma 60', 'Altro');
+insert into Medico values ('CRSSNT95A41L483B', 'Udine', 'Via Garibaldi 1', 'Altro');
 
 
 insert into prenotazione values ('PPIBDA80A01L219Z', null, 'pippobaudo@rai.it', 'Web');
 insert into prenotazione values ('LSSGGI81H03F205A', null, 'agigio@gmail.com', 'Web');
 insert into prenotazione values ('LCUPSL76E52D969X', 0123456789, null, 'App');
 insert into prenotazione values ('MSSBLD45L23L483O', 9876543210,null, 'App');
+insert into prenotazione values ('CRSSNT95A41L483B', null, 's.cristo@esa.eu', 'Web');
 
 
 insert into Convocazione Values ('PPIBDA80A01L219Z', '11:20', TO_DATE('10/05/2021', 'DD/MM/YYYY'), 'OP123', 'Torino', 'Corso palestro 12', 'COVIDIN');
 insert into Convocazione Values ('LSSGGI81H03F205A', '15:30', TO_DATE('20/05/2021', 'DD/MM/YYYY'), 'OP123', 'Milano', 'Corso Svizzera 92', 'CORONAX');
 insert into Convocazione Values ('LCUPSL76E52D969X', '18:00', TO_DATE('11/04/2021', 'DD/MM/YYYY'), 'OP123', 'Genova', 'Via Roma 60', 'FLUSTOP' );
 insert into Convocazione Values ('MSSBLD45L23L483O', '08:00', TO_DATE('10/06/2021', 'DD/MM/YYYY'), 'OP123', 'Udine' , 'Via Garibaldi 1', 'COVIDIN' );
+insert into Convocazione values ('CRSSNT95A41L483B', '12:15', TO_DATE('12/03/2021', 'DD/MM/YYYY'), 'OP567', 'Udine', 'Via Garibaldi 1', 'COVIDIN');
 
 
 insert into Somministrazione values ('LSSGGI81H03F205A', TO_DATE('20/05/2021', 'DD/MM/YYYY'), '15:30' , 'NCLTMB83H03L219S', 'Milano', 'Corso Svizzera 92', 'COV_1234' );
-
+insert into Somministrazione values ('PPIBDA80A01L219Z', TO_DATE('10/05/2021', 'DD/MM/YYYY'), '11:20' , 'PLLFRC80A41L219R', 'Torino', 'Corso palestro 12', 'COR_0122' );
+insert into Somministrazione values ('LCUPSL76E52D969X', TO_DATE('11/04/2021', 'DD/MM/YYYY'), '18:00', 'GNTRMS00A41D969W','Genova', 'Via Roma 60', 'FLU_5678' );
+insert into Somministrazione values ('MSSBLD45L23L483O', TO_DATE('10/06/2021', 'DD/MM/YYYY'), '08:00', 'CRSSNT95A41L483B', 'Udine', 'Via Garibaldi 1', 'COR_0122');
+insert into Somministrazione values ('CRSSNT95A41L483B', TO_DATE('12/03/2021', 'DD/MM/YYYY'), '12:15', 'CRSSNT95A41L483B', 'Udine', 'Via Garibaldi 1', 'COV_1234');
 
 insert into Fiala values ('FCOV123', 'COVIDIN', 'COV_1234');
 insert into Fiala values ('FCOV456', 'COVIDIN', 'COV_1234');
@@ -236,6 +249,40 @@ insert into Disponibilita values('Milano', 'Corso Svizzera 92', 'FCOV456', 'COVI
 insert into Disponibilita values('Milano', 'Corso Svizzera 92', 'FCOV789', 'COVIDIN');
 insert into Disponibilita values('Milano', 'Corso Svizzera 92', 'FFLU123', 'FLUSTOP');
 insert into Disponibilita values('Milano', 'Corso Svizzera 92', 'FCOR123', 'CORONAX');
-
-
 insert into Disponibilita values('Milano', 'Corso Svizzera 92', 'FCOV123', 'COVIDIN');
+
+
+--operazioni sql
+
+--per ogni centro vaccinale elencare le fiale disponibili ancora
+select Citta_Centro_Vaccinale Città_centro, Indirizzo_Centro_Vaccinale Indirizzo_Centro, count(*) Fiale_disponibili
+from Disponibilita
+group by Disponibilita.Indirizzo_Centro_Vaccinale, Disponibilita.Citta_Centro_Vaccinale;
+
+
+
+--Numero di somministrazioni per categoria di Vaccinati
+
+select cittadino.categoria Categoria, count(*) Somministrazioni_fatte
+from Somministrazione join Cittadino on Cittadino.Codice_Fiscale = Somministrazione.Codice_Fiscale
+group by Cittadino.Categoria;
+
+
+--contare quante prenotazioni sono state fatte via web e via app
+
+select Origine_Prenotazione, count(*) Conteggio
+from prenotazione
+group by Origine_Prenotazione;
+
+
+--mostrare i report allergie fatti dal medico Federica pellegrini
+select Cittadino.Nome, Cittadino.Cognome,  ReportAllergie.Descrizione_Reazione
+from ReportAllergie join Cittadino on Cittadino.Codice_Fiscale = ReportAllergie.Codice_Fiscale
+where ReportAllergie.Medico_Codice_Fiscale like 'PLLFRC%';
+
+
+
+--mostrare quante somministrazioni sono state fatte per ogni lotto
+select Lotto.id_lotto, COALESCE(count(Somministrazione.*), 0)
+from Somministrazione right join Lotto on Lotto.id_lotto = Somministrazione.Lotto_ID
+group by Lotto.id_lotto;
